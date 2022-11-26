3269362684a92c320235142bd11288e7a8ddb1c5
Toby Davis <pencilcaseman@gmail.com>
2022-11-26 00:51:37 +0000

    Initial Commit

--- main.ts
+++ main.ts
@@ -1,81 +1,39 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {App, MarkdownPostProcessorContext, MarkdownView, Plugin, PluginManifest} from "obsidian";
 
-// Remember to rename these classes and interfaces!
+// Import XMLHttpRequest
+import {XMLHttpRequest} from "xmlhttprequest";
 
-interface MyPluginSettings {
+interface CodeRunnerSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: CodeRunnerSettings = {
+	mySetting: "default"
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class CodeRunnerPlugin extends Plugin {
+	settings: CodeRunnerSettings;
+	languages: object;
+	compilers: object;
+
+	constructor(app: App, pluginManifest: PluginManifest) {
+		super(app, pluginManifest);
+	}
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		// Load the available languages and compilers
+		this.languages = await getLanguages();
+		this.compilers = await getCompilers();
+
+		console.log(this.languages);
+		console.log(this.compilers);
+
+		// Register the CodeBlock formatter
+		this.registerMarkdownPostProcessor((element, context) => {
+			codeBlockProcessor(element, context, this.app, this);
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -91,47 +49,83 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+function extractCodeData(element: HTMLElement, context: MarkdownPostProcessorContext, app: App) {
+	const codeBlock = context.getSectionInfo(element);
+
+	if (!codeBlock) {
+		return {"valid": false};
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+	let fullProgram = "";
+	for (let i: number = codeBlock.lineStart; i < codeBlock.lineEnd; ++i) {
+		fullProgram += app.workspace.getActiveViewOfType(MarkdownView)?.editor.getLine(i);
 	}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+	// Locate the <compile> ... </compile> tags
+	const compileTagMatch = fullProgram.match(/<compile>(.*?)<\/compile>/g);
+	if (!compileTagMatch) {
+		return {"valid": false};
 	}
+
+	const compileTagString = compileTagMatch[0];
+	const code = fullProgram.substring(fullProgram.indexOf("</compile>") + 10);
+	const config = JSON.parse(compileTagString.substring(9, compileTagString.length - 10));
+
+	return {"valid": true, "code": code, "config": config}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+function codeBlockProcessor(
+	element: HTMLElement,
+	context: MarkdownPostProcessorContext,
+	app: App,
+	plugin: CodeRunnerPlugin
+) {
+	const codeData = extractCodeData(element, context, app);
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
+	if (!codeData.valid) {
+		return;
 	}
 
-	display(): void {
-		const {containerEl} = this;
+	console.log("Extracted information:");
+	console.log(codeData);
+	console.log(codeData.code);
+}
 
-		containerEl.empty();
+function restfulApiRequest(method: string, url: string, data: object): Promise<any> {
+	let toSend = null;
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+	if (method === "GET") {
+		toSend = {
+			method: method,
+			headers: {
+				"Accept": "application/json",
+				"Content-Type": "application/json"
+			}
+		};
+	} else {
+		toSend = {
+			method: method,
+			headers: {
+				"Accept": "application/json",
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(data)
+		}
+	}
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+	return fetch("https://godbolt.org" + url, toSend).then(response => response.json()).then(data => {
+		return data;
+	});
+}
+
+async function getLanguages(): Promise<object> {
+	return restfulApiRequest("GET", "/api/languages", {});
+}
+
+async function getCompilers(languageID?: string): Promise<object> {
+	if (languageID) {
+		return restfulApiRequest("GET", "/api/compilers/" + languageID, {});
+	} else {
+		return restfulApiRequest("GET", "/api/compilers", {});
 	}
 }
